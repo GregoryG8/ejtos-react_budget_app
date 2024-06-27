54e95265150ca09632d2aa52569c6dbf2b4f61f6
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-06-27 16:58:50 -0500

    Task 3 done

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,49 +1,68 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState, useEffect } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-    const { budget, expenses, dispatch } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const [error, setError] = useState('');
-    
-    useEffect(() => {
-        setNewBudget(budget);
-    }, [budget]);
+  const { budget, expenses, dispatch, currency } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+  const [localCurrency, setLocalCurrency] = useState(currency);
 
-    const handleBudgetChange = (event) => {
-        const value = Number(event.target.value);
+  useEffect(() => {
+    setNewBudget(budget);
+  }, [budget]);
 
-         if (value > 20000) {
-            alert("Budget cannot exceed £20,000.");
-        } else {
-            setNewBudget(value);
-        }
-    };
+  useEffect(() => {
+    setLocalCurrency(currency);
+  }, [currency]);
 
-    const handleBlur = () => {
-        const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
-        if (newBudget < totalExpenses) {
-            alert("You cannot reduce the budget value lower than the spending.");
-            
-        }else{
-            dispatch({ type: 'SET_BUDGET', payload: newBudget });
-        }
-    };
+  const handleBudgetChange = (event) => {
+    const value = Number(event.target.value);
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-            <input 
-                type="number" 
-                step="10" 
-                min={"0"}
-                max={"20000"} 
-                value={newBudget} 
-                onChange={handleBudgetChange} 
-                onBlur={handleBlur}
-            />
-        </div>
+    if (value > 20000) {
+      alert("Budget cannot exceed £20,000.");
+    } else {
+      setNewBudget(value);
+    }
+  };
+
+  const handleCurrencyChange = (event) => {
+    const newCurrency = event.target.value;
+    setLocalCurrency(newCurrency);
+    dispatch({ type: "CHG_CURRENCY", payload: newCurrency });
+  };
+
+  const handleBlur = () => {
+    const totalExpenses = expenses.reduce(
+      (total, item) => total + item.cost,
+      0
     );
+    if (newBudget < totalExpenses) {
+      alert("You cannot reduce the budget value lower than the spending.");
+    } else {
+      dispatch({ type: "SET_BUDGET", payload: newBudget });
+    }
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: </span>
+      <input
+        type="number"
+        step="10"
+        min={"0"}
+        max={"20000"}
+        value={newBudget}
+        onChange={handleBudgetChange}
+        onBlur={handleBlur}
+      />
+      <label htmlFor="currency">Currency: </label>
+      <select id="currency" value={currency} onChange={handleCurrencyChange}>
+        <option value="$">$ Dollar</option>
+        <option value="£">£ Pound</option>
+        <option value="€">€ Euro</option>
+        <option value="₹">₹ Ruppee</option>
+      </select>
+    </div>
+  );
 };
 
 export default Budget;
